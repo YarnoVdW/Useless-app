f50bc8b03cb6817269bf9e593e21da211412115e
YarnoVdW <95145420+YarnoVdW@users.noreply.github.com>
2022-01-31 17:36:10 +0100

    Initial Commit

---
+++ RandomPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by Yarno Van de Weyer on 31/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
