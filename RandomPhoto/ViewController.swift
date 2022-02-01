e05bb39249cf55013eb27063a577331ac088b3ae
YarnoVdW <95145420+YarnoVdW@users.noreply.github.com>
2022-02-01 13:54:56 +0100

    This is a useless app that I made.
    Push the button and see the magic happen

--- RandomPhoto/ViewController.swift
+++ RandomPhoto/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  RandomPhoto
+//  UselessApp
 //
 //  Created by Yarno Van de Weyer on 31/01/2022.
 //
@@ -9,52 +9,68 @@ import UIKit
 
 class ViewController: UIViewController {
     private let imageView: UIImageView = {
-       let imageView = UIImageView()
+        let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
         imageView.backgroundColor = .white
+        
         return imageView
     }()
-    let collorAray = [ UIColor.red, UIColor.blue, UIColor.green, UIColor.magenta]
+    
+    
     
     let button = UIButton()
     
     func configureButton() {
-        button.configuration = .tinted()
-        button.configuration?.title = "pres me"
-        button.configuration?.image = UIImage(systemName: "questionmark")
+        button.configuration = .filled()
+        button.configuration?.title = "press me"
+        button.configuration?.image = UIImage(systemName: "camera")
         button.configuration?.baseBackgroundColor = .systemBlue
-        button.configuration?.imagePadding = 6
-        button.configuration?.baseForegroundColor = .systemBlue
+        button.configuration?.imagePadding = 8
+        button.configuration?.baseForegroundColor = .black
+        button.configuration?.imagePlacement = .leading
+        button.configuration?.cornerStyle = .capsule
+        
+        buttonConstraint()
+        
+    }
+    func buttonConstraint() {
+        view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                                    
+                                     button.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 250),
+                                     button.widthAnchor.constraint(equalToConstant: 150),
+                                     button.heightAnchor.constraint(equalToConstant: 70)])
     }
         
     
 
     override func viewDidLoad() {
             super.viewDidLoad()
-            view.backgroundColor = collorAray.randomElement()
-           
             view.addSubview(imageView)
             imageView.frame = CGRect(x: 0, y: 0, width: 900, height: 1800)
             imageView.center = view.center
         
             view.addSubview(button)
-            //button.frame = CGRect(x: 10, y: view.frame.size.width-100-view.safeAreaInsets.bottom, width: view.frame.size.width-40, height: 30)
-            getRandomPhoto()
             configureButton()
-            button.addTarget(self, action: #selector(drukken), for: .allTouchEvents)
+            button.addTarget(self, action: #selector(presButton), for: .allTouchEvents)
+            getRandomPhoto()
+        
+        
+        
         }
     
     
-    @objc func drukken() {
+    @objc func presButton() {
         getRandomPhoto()
     }
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        button.frame = CGRect(x: 10, y: view.frame.size.height-100-view.safeAreaInsets.bottom, width: view.frame.size.width-20, height: 50)
-    }
         
+    }
         func getRandomPhoto() {
             let urlString = "https://source.unsplash.com/random/600x600"
+            
             let url = URL(string: urlString)!
             guard let data = try? Data(contentsOf: url) else {
                 return
